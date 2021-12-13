ec73519c499d7a08f82fa9eb8181952ee35ecc4e
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2021-12-14 00:41:32 +1100

    creating method to check valid length, creating method to ask for length

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,44 @@ function writePassword() {
 
 }
 
+function checkValidLength(templength){
+  var length = parseInt(templength);
+  if(length >= 8 && length <= 128 && isNaN(length) !== true){
+      return length;
+  }
+  else{
+    length = parseInt(window.prompt("That is not a valid value. Please enter desired password length (between 8 and 128):"));
+    checkValidLength(length);
+  }
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Random Number generator
+function randNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+// Symbol and Letter arrays
+var alphabet = Array.from('abcdefghijklmnopqrstuvwxyz');
+var symbols = Array.from('!@#$%^&*()_-?<>');
+
+// Usage variables for final pass
+var numCharacters = [];
+var lowerCharacters = [];
+var upperCharacters = [];
+var symbolCharacters = [];
+var passwordLength;
+var password = [];
+
+function generatePassword(){
+  var tempLength = window.prompt("Please enter desired password length (between 8 and 128):");
+  passwordLength = checkValidLength(tempLength);
+}
+
+
 
 
